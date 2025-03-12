b78f6cb09ebfd37998a38553f044a758f12ca7d2
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-03-12 21:37:12 +0530

    cleaned up ws backend logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -10,6 +10,29 @@ const wss = new WebSocketServer({ port: 8080 });
 wss.on('connection', function connection(ws,req) {
   ws.on('error', console.error);
 
+
+function checkUserStatus(token:string): string | null {
+  let decoded:CustomPayload;
+  try{
+    decoded = jwt.verify(token,accessTokenSecret) as CustomPayload
+  }catch(err){
+    console.log('token cant be verified');
+    
+    return null;
+  }
+
+  if(!decoded.id){
+    console.log('decoded token does not contain Username')
+    
+    return null;
+  }
+
+
+ console.log('the loggged in username is:',decoded.User)
+  return decoded.id
+
+}
+
 const url = req.url;
 
 if(!url){
@@ -18,24 +41,12 @@ if(!url){
 const queryParameters = new URLSearchParams(url.split('?')[1]);
 const token = queryParameters.get('token') ?? 'default_token';
 
-let decoded:CustomPayload;
-try{
-  decoded = jwt.verify(token,accessTokenSecret) as CustomPayload
-}catch(err){
-  console.log('token cant be verified');
-  ws.close();
-  return;
-}
 
 
 
-if(!decoded.User){
-  console.log('decoded token does not contain Username')
-  ws.close()
-  return
-}
 
-console.log('the loggged in username is:',decoded.User)
+
+
  
 
 
--- apps/ws-backend/types/jwt.d.ts
+++ apps/ws-backend/types/jwt.d.ts
@@ -1,3 +1,5 @@
 interface CustomPayload{
     User:string;
+    id:string;
+    
 }
