859039d0fa0264999a594a04f20108c835fa5142
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-04-15 21:36:03 +0530

    [refactor:] improved button styling

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,8 +6,11 @@ import { Button } from "@repo/ui/button";
 
 export default function Home() {
   return (
-    <div className ="border-red-100 border-4">
-     <Button text={"First Button"} onClick={()=>alert('button clicked')}></Button>
+    <div className ="border-red-100 border-4 bg-black h-28 p-2">
+     <Button text={"Sign Up"} variant={"primary"} onClick={()=>alert('button clicked')}></Button>
+     <Button text={"Log In"} variant={"secondary"} onClick={()=>alert('button clicked')}></Button>
+     <Button text={"Third Button"} variant={"primary"} size={"small"} onClick={()=>alert('button clicked')}></Button>
+     <Button text={"Fourth Button"} variant={"secondary"} size={"small"} onClick={()=>alert('button clicked')}></Button>
     </div>
   );
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,15 +3,15 @@ import { cn } from './utils'
 
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
-const buttonVariants = cva("rounded-full border-2",{
+const buttonVariants = cva("inline-flex items-center justify-center font-medium transition-all duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 rounded-full border-2 gap-2 shadow-sm hover:shadow active:shadow-inner",{
   variants:{
     variant:{
-      primary:" bg-slate-300 border-slate-300 text-black",
-      secondary:"bg-black border-slate-300 text-slate-300"
+      primary:" bg-white border-gray-300 text-black hover:bg-gray-100 hover:border-gray-400 active:bg-gray-200 focus:ring-gray-300",
+      secondary:"bg-black border-gray-600  text-white hover:bg-gray-900 hover:border-gray-400 active:bg-gray-700 focus:ring-gray-600"
     },
     size:{
-      small:"p-3 text-md",
-      large:"p-6 text-lg"
+      small:" h-9 px-3 py-2 text-sm min-w-24",
+      large:"h-11 px-5 py-2.5 text-base min-w-32"
     }
   },
   defaultVariants:{
